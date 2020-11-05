285a7c5d29ca1560168f8bcb6fc854686a52928d
SnhzGonz <gonzaloemanuel.gs@gmail.com>
2020-11-04 21:30:54 -0300

    Entrega final

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,3 +1,33 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea
+class Tarea (val horasEstimadas: Int, var responsable: Empleado,
+             var empleadosAsignados: List<Empleado>, var costoInfraestructura: Int){
+
+    fun nominaDeEmpleados() = empleadosAsignados + responsable
+
+    fun horasNecesariasParaFinalizar() = horasEstimadas / empleadosAsignados.size
+    fun sumaDeSalarioDeEmpleados() = empleadosAsignados.sumBy { it.sueldoPorHora * this.horasNecesariasParaFinalizar()}
+    fun salarioDeResponsable() = responsable.sueldoPorHora * horasEstimadas
+
+    fun costoDeTarea() = this.sumaDeSalarioDeEmpleados() + this.salarioDeResponsable() + costoInfraestructura
+
+
+}
+
+class TareaDeIntegración(val tareasACoordinar: List<Tarea>, var responsable: Empleado){
+    fun sumaDeHorasDeSubtareas() = tareasACoordinar.sumBy { it.horasNecesariasParaFinalizar() }
+    fun calculoDeHorasDePlanificacion() = this.sumaDeHorasDeSubtareas() / 8
+
+    fun horasNecesariasParaTerminar() = this.sumaDeHorasDeSubtareas() + this.calculoDeHorasDePlanificacion()
+
+    fun sumaDeCostosDeSubtareas() = tareasACoordinar.sumBy { it.costoDeTarea() }
+    fun sueldoDeResponsable() = this.sumaDeCostosDeSubtareas() * 0.03
+
+    fun costoDeTarea() = this.sumaDeCostosDeSubtareas() + this.sueldoDeResponsable()
+
+    fun nominasDeSubtareas() = tareasACoordinar.map { it.nominaDeEmpleados() }
+
+    fun nominaDeTarea() = this.nominasDeSubtareas() + responsable
+}
+
+class Empleado (val sueldoPorHora: Int) {}
--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -1,8 +1,67 @@
 package ar.edu.unahur.obj2.tareas
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldContainAll
+import io.kotest.matchers.shouldBe
 
 class TareaTest : DescribeSpec({
   describe("Una tarea") {
+    // empleados
+    var Juan = Empleado(200)
+    var Pedro = Empleado(300)
+    var Marcela = Empleado(100)
+    var Laura = Empleado(400)
+    var Carolina = Empleado(300)
+    var Carlos = Empleado(500)
+    var Veronica = Empleado(200)
+    var BigBoss = Empleado(1000)
+
+    // tareas
+    var TareaUno = Tarea(30, Laura, listOf(Marcela, Carlos, Pedro, Juan, Veronica), 9000)
+    var TareaDos = Tarea(24, Juan, listOf(Veronica, Laura, Carlos, Carolina), 8000)
+    var TareaTres = Tarea(27, Veronica, listOf(Juan, Carolina, Pedro), 5000)
+
+    // tarea integracion
+    var TareaMadre = TareaDeIntegración(listOf(TareaUno, TareaTres), BigBoss)
+
+    // requerimiento 1
+    it("la nómina de la tarea uno es (Maercela, Carlos, Pedro, Juan, Veronica, Laura)"){
+      var listaAux = listOf<Empleado>(Marcela, Carlos, Pedro, Juan, Veronica, Laura)
+      TareaUno.nominaDeEmpleados().shouldContainAll(listaAux)
+    }
+    it("la nómina de la tarea tres es (Juan, Carolina, Pedro, Veronica)"){
+      var listaAux = listOf<Empleado>(Juan, Carolina, Pedro, Veronica)
+      TareaTres.nominaDeEmpleados().shouldContainAll(listaAux)
+    }
+
+    // requerimiento 2
+    it("para la tarea tres se necesitan 9 horas"){
+      TareaTres.horasNecesariasParaFinalizar().shouldBe(9)
+    }
+    it("para la tarea dos se necesitan 6 horas"){
+      TareaDos.horasNecesariasParaFinalizar().shouldBe(6)
+    }
+
+    // requerimiento 3
+    it("el costo de la tarea dos es de 21200"){
+      TareaDos.costoDeTarea().shouldBe(21200)
+    }
+    it("el costo de la tarea uno es de 28800"){
+      TareaUno.costoDeTarea().shouldBe(28800)
+    }
+
+    // requerimiento 4
+    it("La tarea madre necesita 16 horas para realizarse"){
+      TareaMadre.horasNecesariasParaTerminar().shouldBe(16)
+    }
+    it("El costo total de la tarea madre es de 47792"){
+      TareaMadre.costoDeTarea().shouldBe(47792)
+    }
+    it("La nomina de la tarea madre es: (Marcela, Carlos, Pedro, Juan, Veronica, Laura), (Juan, Carolina, Pedro, Veronica), y BigBoss"){
+      var listaAux = listOf(listOf(Marcela, Carlos, Pedro, Juan, Veronica, Laura),
+                            listOf(Juan, Carolina, Pedro, Veronica),
+                            BigBoss)
+      TareaMadre.nominaDeTarea().shouldContainAll(listaAux)
+    }
   }
 })
